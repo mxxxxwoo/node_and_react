5fe077bd250f655e90302b323d584a96652a7071
mengkko <myungwoo@mengkko.dev>
2020-06-18 11:52:47 +0900

    react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
 function App() {
-    return <div className="App">App App</div>
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={LandingPage}></Route>
+                    <Route exact path="/login" component={LoginPage}></Route>
+                    <Route exact path="/register" component={RegisterPage}></Route>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
 export default App
